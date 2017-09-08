b7127b76091838204fa6e888e7146ce01bed8698
alekseynp <me@alekseynp.com>
2017-09-08 12:57:08 -0700

    Gaussing Blur module in processing

--- python/processing/blur.py
+++ python/processing/blur.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import torch.nn as nn
 from models.median_pool import MedianPool2d
 
@@ -34,3 +35,48 @@ class Blur(nn.Module):
     def forward(self, x):
         blurred = self.median_pool(x)
         return blurred
+
+
+class GaussianBlur(nn.Module):
+    def __init__(self, kernel_size, sigma, same=True):
+        super(GaussianBlur, self).__init__()
+
+        self.same = same
+        if self.same:
+            self.padding = nn.ReplicationPad2d(1).cuda()
+
+        self.convolution = nn.Conv2d(3,3,kernel_size,stride=1,padding=0,groups=3,bias=False)
+
+        kernel = GaussianBlur.matlab_style_gauss2D(
+            shape=(kernel_size, kernel_size),
+            sigma=sigma
+        )
+        convolution_weight_numpy = np.stack([kernel[None, :, :] for _ in range(3)])
+        self.convolution.weight = nn.Parameter(torch.FloatTensor(convolution_weight_numpy))
+        self.convolution.cuda()
+
+    def forward(self, x):
+        if self.same:
+            padded = self.padding(x)
+        else:
+            padded = x
+        blurred = self.convolution(padded)
+        return blurred
+
+
+    # From https://stackoverflow.com/questions/17190649/how-to-obtain-a-gaussian-filter-in-python
+    # Not clear how this differs from other implementations
+    @staticmethod
+    def matlab_style_gauss2D(shape=(3, 3), sigma=0.5):
+        """
+        2D gaussian mask - should give the same result as MATLAB's
+        fspecial('gaussian',[shape],[sigma])
+        """
+        m, n = [(ss - 1.) / 2. for ss in shape]
+        y, x = np.ogrid[-m:m + 1, -n:n + 1]
+        h = np.exp(-(x * x + y * y) / (2. * sigma * sigma))
+        h[h < np.finfo(h.dtype).eps * h.max()] = 0
+        sumh = h.sum()
+        if sumh != 0:
+            h /= sumh
+        return h
