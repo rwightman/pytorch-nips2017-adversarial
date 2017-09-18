95c61d66fbedfc3d18ff0f6be113e1b494a26693
alekseynp <me@alekseynp.com>
2017-09-18 01:04:51 -0700

    Current universal training script

--- python/train_universal.py
+++ python/train_universal.py
@@ -13,34 +13,48 @@ import torch.optim as optim
 from tensorboardX import SummaryWriter
 import processing
 import torchvision.utils
+from scipy.misc import imsave
 
 with open('local_config.yaml', 'r') as f:
     local_config = yaml.load(f)
 CHECKPOINT_DIR = local_config['checkpoints_dir']
 
 input_dir = '/media/stuff/ImageNet/train'
-ensemble = ['adv_inception_resnet_v2', 'inception_v3', 'resnet18']
-ensemble_weights = [1.0, 1.0, 0.5]
-checkpoint_paths = [
-    os.path.join(CHECKPOINT_DIR, 'adv_inception_resnet_v2.pth'),
-    os.path.join(CHECKPOINT_DIR, 'inception_v3_google-1a9a5a14.pth'),
-    os.path.join(CHECKPOINT_DIR, 'resnet18-5c106cde.pth')
-]
+
+TARGET_CLASS = 851
+
 max_epsilon = 16.0
-batch_size = 12
-img_size = 299
+batch_size = 4
+
+ensemble = ['adv_inception_resnet_v2', 'inception_v3_tf', 'adv_inception_v3', 'resnet101', 'dpn68', 'densenet161', 'alexnet']
+ensemble_weights = [1.0, 1.5, 1.0, 0.5, 0.75, 0.75, 0.5]
+
+"""
+
+#Just inception v3
+from models.model_configs import config_from_string
+ensemble = ['adv_inception_v3']
+ensemble_weights = [1.0]
+"""
+
+
+
+checkpoint_paths = [os.path.join(CHECKPOINT_DIR, config_from_string(m)['checkpoint_file']) for m in ensemble]
+
+out_filename = '{}_{}_eps{}'.format(''.join(ensemble), TARGET_CLASS, max_epsilon)
+
 
 dataset = Dataset(input_dir)
 
+img_size = 299
 cfgs = [config_from_string(s) for s in ensemble]
-
 target_model = create_ensemble(cfgs, ensemble_weights, checkpoint_paths)
 
 for transformed_model in target_model.models:
     transformed_model.model.cuda()
     transformed_model.model.eval()
 
-eps = max_epsilon / 256.0
+eps = max_epsilon / 255.0
 
 loader = data.DataLoader(
     dataset,
@@ -82,6 +96,9 @@ augmentation = nn.Sequential(
     processing.RandomCrop()
 )
 
+# Uncomment this line to disable augmentation simply
+#augmentation = lambda x: x
+
 perturbation_model = PerturbationNet(
     target_model,
     augmentation,
@@ -94,32 +111,40 @@ w_matrix = autograd.Variable(
     torch.zeros(1, 3, img_size, img_size).cuda(),
     requires_grad=True)
 perturbation_model.set_w_matrix(w_matrix)
-optimizer = optim.Adam([w_matrix], lr=0.04)
+optimizer = optim.Adam([w_matrix], lr=0.08)
 
 writer = SummaryWriter()
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-lr_reducers = ReduceLROnPlateau(optimizer, factor=0.5, verbose=True, patience=5)
-
-n_batches_per_epoch = 400
+n_batches_per_epoch = 100
 epoch_batch_idx = 0
 losses = []
 for batch_idx, (input, class_id) in enumerate(loader):
     if epoch_batch_idx == n_batches_per_epoch:
         mean_loss = np.mean(losses)
-        lr_reducers.step(mean_loss)
         epoch_batch_idx = 0
         losses = []
-        img = torch.tanh(perturbation_model.w_matrix).data.cpu()
-        writer.add_image('Image', torchvision.utils.make_grid(img, normalize=True), batch_idx)
+
+        w_matrix_to_save = perturbation_model.w_matrix.data.cpu()
+        delta = torch.tanh(w_matrix_to_save)
+        writer.add_image('Image', torchvision.utils.make_grid(delta, normalize=True), batch_idx)
+
+        np.save('../univs/{}_iter{}'.format(out_filename,batch_idx), w_matrix_to_save.numpy())
+
+        imsave('../univs/{}_iter{}.png'.format(out_filename,batch_idx),
+               np.round(255.0 * np.transpose(delta[0].numpy()*0.5+0.5, axes=(1, 2, 0))).astype(np.uint8),
+               format='png')
+
+        writer.add_scalar('grad/magnitude', float(np.linalg.norm(perturbation_model.w_matrix.grad.data.cpu().numpy())), batch_idx)
+        writer.add_histogram('image/hist', w_matrix_to_save.numpy(), batch_idx)
+
     epoch_batch_idx = epoch_batch_idx + 1
     input = input.cuda()
     input_var = autograd.Variable(input, volatile=False, requires_grad=True)
-    class_var = autograd.Variable(torch.LongTensor(np.repeat(506,batch_size))).cuda()
+    class_var = autograd.Variable(torch.LongTensor(np.repeat(TARGET_CLASS,batch_size))).cuda()
 
-    probs_perturbed_var = perturbation_model(input_var)
+    log_probs_perturbed_var = perturbation_model(input_var)
     optimizer.zero_grad()
-    loss = nllloss(torch.log(probs_perturbed_var + 1e-8), target=class_var)
+    loss = nllloss(log_probs_perturbed_var, target=class_var)
     losses.append(loss.data.cpu().numpy())
     loss.backward()
 
@@ -128,7 +153,7 @@ for batch_idx, (input, class_id) in enumerate(loader):
     optimizer.step()
 
 
-np.save('{}_coil'.format(''.join(ensemble)), perturbation_model.w_matrix.data.cpu().numpy())
+np.save(out_filename, perturbation_model.w_matrix.data.cpu().numpy())
 
 
 #from PIL import Image
