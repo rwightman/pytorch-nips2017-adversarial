88b9a48accded497ff0149c600985a24cb4f5373
alekseynp <me@alekseynp.com>
2017-10-04 20:23:06 -0700

    Change CWInspired features, did not yet update runner.

--- python/attacks/cw_inspired.py
+++ python/attacks/cw_inspired.py
@@ -55,7 +55,9 @@ class CWInspired(object):
                  target_nth_highest=6,
                  prob_dont_augment=0.0,
                  initial_w_matrix=None,
-                 always_target=None):
+                 always_target=None,
+                 random_start=None,
+                 n_restarts=1):
         super(CWInspired, self).__init__()
         self.eps = max_epsilon / 255.0
         self.n_iter = n_iter
@@ -74,6 +76,8 @@ class CWInspired(object):
             prob_dont_augment,
         ).cuda()
         self.loss_fn = torch.nn.NLLLoss().cuda()
+        self.random_start = random_start
+        self.n_restarts = n_restarts
 
 
     def __call__(self, input, target, batch_idx, deadline_time):
@@ -86,19 +90,8 @@ class CWInspired(object):
         # In case of the final batch not being complete
         this_batch_size = input_var.size(0)
 
-        if self.initial_w_matrix is None:
-            batch_w_matrix = autograd.Variable(
-                torch.zeros(input.size()).cuda(),
-                requires_grad=True)
-        else:
-            batch_w_matrix = autograd.Variable(
-                torch.FloatTensor(
-                    np.stack([self.initial_w_matrix[0] for _ in range(this_batch_size)])).cuda(),
-                requires_grad=True)
-
-        self.perturbation_model.set_w_matrix(batch_w_matrix)
-
         # Predict class
+        self.perturbation_model.set_w_matrix(autograd.Variable(torch.zeros(input.size()).cuda(),requires_grad=True))
         if not self.targeted:
             if self.always_target is not None:
                 target = torch.LongTensor(np.repeat(self.always_target,this_batch_size))
@@ -113,33 +106,51 @@ class CWInspired(object):
         # target came either from the loader or above
         target_var = autograd.Variable(target).cuda()
 
-        optimizer = optim.Adam([batch_w_matrix], lr=self.lr)
-
         best_loss = torch.FloatTensor(np.repeat(9999.0, this_batch_size)).cuda()
-        best_w_matrix = autograd.Variable(torch.zeros(batch_w_matrix.size()).cuda())
+        best_w_matrix = autograd.Variable(torch.zeros(input.size()).cuda())
 
-        for i in range(self.n_iter):
-            log_probs_perturbed_var = self.perturbation_model(input_var)
-            optimizer.zero_grad()
-            loss = self.loss_fn(log_probs_perturbed_var, target=target_var)
+        for _ in range(self.n_restarts):
 
-            better = loss.data < best_loss
-            for b in range(this_batch_size):
-                if better[b]:
-                    best_w_matrix[b, :, :, :] = batch_w_matrix[b, :, :, :]
+            if self.initial_w_matrix is None:
+                batch_w_matrix = autograd.Variable(
+                    torch.zeros(input.size()).cuda(),
+                    requires_grad=True)
+            elif self.random_start:
+                batch_w_matrix = autograd.Variable(
+                    torch.FloatTensor(np.random.uniform(self.random_start, list(input.size()))).cuda(),
+                    requires_grad=True)
+            else:
+                batch_w_matrix = autograd.Variable(
+                    torch.FloatTensor(
+                        np.stack([self.initial_w_matrix[0] for _ in range(this_batch_size)])).cuda(),
+                    requires_grad=True)
 
-            loss.backward()
-            optimizer.step()
+            self.perturbation_model.set_w_matrix(batch_w_matrix)
 
-            time_remaining = deadline_time - time.time()
-            if i > 10 and time_remaining < time_thresh // 2:
-                print("Warning: breaking early at %d, time critical, %s remaining in batch."
-                      % (i, time_remaining))
-                sys.stdout.flush()
-                break
-            elif time_remaining < time_thresh:
-                print("Warning: time critical, %s remaining in batch." % time_remaining)
-                sys.stdout.flush()
+            optimizer = optim.Adam([batch_w_matrix], lr=self.lr)
+
+            for i in range(self.n_iter):
+                log_probs_perturbed_var = self.perturbation_model(input_var)
+                optimizer.zero_grad()
+                loss = self.loss_fn(log_probs_perturbed_var, target=target_var)
+
+                better = loss.data < best_loss
+                for b in range(this_batch_size):
+                    if better[b]:
+                        best_w_matrix[b, :, :, :] = batch_w_matrix[b, :, :, :]
+
+                loss.backward()
+                optimizer.step()
+
+                time_remaining = deadline_time - time.time()
+                if i > 10 and time_remaining < time_thresh // 2:
+                    print("Warning: breaking early at %d, time critical, %s remaining in batch."
+                          % (i, time_remaining))
+                    sys.stdout.flush()
+                    break
+                elif time_remaining < time_thresh:
+                    print("Warning: time critical, %s remaining in batch." % time_remaining)
+                    sys.stdout.flush()
 
         final_change = PerturbationNet.delta(best_w_matrix, input_var, self.eps)
         final_change = torch.clamp(final_change, -self.eps, self.eps)  # Hygiene, math should mean this is already true
