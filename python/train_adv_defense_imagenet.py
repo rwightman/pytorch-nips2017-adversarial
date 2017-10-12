c41de7dcc42070df42a30929ec237ccfbd1fe2bf
alekseynp <me@alekseynp.com>
2017-10-12 00:12:37 -0700

    Re-arranged stuff to make this easier to re-use.

--- python/train_adv_defense_imagenet.py
+++ python/train_adv_defense_imagenet.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import argparse
 import math
@@ -18,40 +17,27 @@ from mp_feeder import MpFeeder
 from defenses import multi_task
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR', help='path to dataset')
 parser.add_argument('--mp', action='store_true', default=False,
                     help='multi-process training, attack and defense in separate processes')
-parser.add_argument('--num-gpu', default=1, type=int, metavar='N',
-                    help='number of gpus to use (default: 1)')
+parser.add_argument('--num-gpu', default=1, type=int, metavar='N', help='number of gpus to use (default: 1)')
 parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=100, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--decay-epochs', type=int, default=15, metavar='N',
-                    help='epoch interval to decay LR')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=128, type=int,
-                    metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--img-size', default=299, type=int,
-                    metavar='N', help='Input image dimension')
-parser.add_argument('--opt', default='sgd', type=str,
-                    metavar='OPT', help='optimizer')
-parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
-                    metavar='LR', help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)')
-parser.add_argument('--print-freq', '-p', default=10, type=int,
-                    metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('--mt', action='store_true', default=False,
-                    help='multi-task defense objective')
-parser.add_argument('--co', action='store_true', default=False,
-                    help='optimize only defense classifier(s) parameters')
-parser.add_argument('--df', action='store_true', default=False,
-                    help='dogfood attack with defense model')
+parser.add_argument('--epochs', default=100, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--decay-epochs', type=int, default=15, metavar='N', help='epoch interval to decay LR')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=16, type=int, metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--img-size', default=299, type=int, metavar='N', help='Input image dimension')
+parser.add_argument('--opt', default='sgd', type=str, metavar='OPT', help='optimizer')
+parser.add_argument('--lr', '--learning-rate', default=0.01, type=float, metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float, metavar='W',
+                    help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=10, type=int, metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('--mt', action='store_true', default=False, help='multi-task defense objective')
+parser.add_argument('--co', action='store_true', default=False, help='optimize only defense classifier(s) parameters')
+parser.add_argument('--df', action='store_true', default=False, help='dogfood attack with defense model')
 
 
 def train(args, train_loader, model, criterion, optimizer, epoch):
@@ -93,14 +79,13 @@ def train(args, train_loader, model, criterion, optimizer, epoch):
         target_adv_var = torch.autograd.Variable(target_adv)
         is_adv_var = torch.autograd.Variable(is_adv)
 
-        #print(i, target_var.data)
-        #utils.save_image(input_var.data, '%d-input.jpg' % i, padding=0)
+        # print(i, target_var.data)
+        # utils.save_image(input_var.data, '%d-input.jpg' % i, padding=0)
 
         # compute output
         output = model(input_var)
         if isinstance(output, tuple):
-            loss = multi_task.multi_loss(
-                output, target_var, target_adv_var, is_adv_var, criterion)
+            loss = multi_task.multi_loss(output, target_var, target_adv_var, is_adv_var, criterion)
             output = output[0]
         else:
             loss = criterion(output, target_var)
@@ -121,29 +106,19 @@ def train(args, train_loader, model, criterion, optimizer, epoch):
         end = time.time()
 
         if i % args.print_freq == 0:
-            sys.stdout.write('\r')
-            sys.stdout.write('\x1b[1A')
-            sys.stdout.write('\r')
-            sys.stdout.write('Epoch: [{0}][{1}/{2}] '
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f}) '
-                  'Data {data_time.val:.2f} ({data_time.avg:.2f}) '
-                  'Loss {loss.val:.4f} ({loss.avg:.4f}) '
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
-                epoch, i, len(train_loader), batch_time=batch_time,
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(epoch, i, len(train_loader), batch_time=batch_time,
                 data_time=data_time, loss=losses, top1=top1, top5=top5))
-            sys.stdout.flush()
 
         if i % 1000 == 0:
-            save_checkpoint({
-                     'epoch': epoch + 1,
-                     'arch': 'FIXME',
-                     'state_dict': model.state_dict(),
-                     'best_prec1': top1.avg,
-                     'optimizer': optimizer.state_dict(),
-                 },
-                 False,
-                 filename='checkpoint-%d.pth.tar' % epoch)
- 
+            save_checkpoint(
+                {'epoch': epoch + 1, 'arch': 'FIXME', 'state_dict': model.state_dict(), 'best_prec1': top1.avg,
+                    'optimizer': optimizer.state_dict(), }, False, filename='checkpoint-%d.pth.tar' % epoch)
+
 
 def validate(args, val_loader, model, criterion):
     batch_time = AverageMeter()
@@ -178,20 +153,14 @@ def validate(args, val_loader, model, criterion):
         end = time.time()
 
         if i % args.print_freq == 0:
-            sys.stdout.write('\r')
-            sys.stdout.write('\x1b[1A')
-            sys.stdout.write('\r')
-            sys.stdout.write('Test: [{0}/{1}]\t'
+            print('Test: [{0}/{1}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                i, len(val_loader), batch_time=batch_time, loss=losses,
-                top1=top1, top5=top5))
-            sys.stdout.flush()
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(i, len(val_loader), batch_time=batch_time,
+                loss=losses, top1=top1, top5=top5))
 
-    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
-          .format(top1=top1, top5=top5))
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
 
     return top1.avg
 
@@ -212,6 +181,48 @@ def get_opt_params(module, classifier_only=False):
 def main():
     args = parser.parse_args()
 
+    train_dir = os.path.join(args.data, 'train')
+    val_dir = os.path.join(args.data, 'validation')
+
+    train_dataset = datasets.ImageFolder(train_dir, transforms.Compose(
+        [transforms.RandomSizedCrop(args.img_size), transforms.RandomHorizontalFlip(), transforms.ToTensor()]))
+
+    attack_cfgs = [{'attack_name': 'iterative', 'targeted': True, 'num_steps': 10, 'target_rand': True},
+        {'attack_name': 'iterative', 'targeted': False, 'num_steps': 1, 'random_start': True},
+        {'attack_name': 'cw_inspired', 'targeted': True, 'n_iter': 38},
+        {'attack_name': 'cw_inspired', 'targeted': False, 'n_iter': 38}, ]
+
+    attack_model_cfgs = [  # FIXME these are currently just test configs, need to setup properly
+        {'models': ['inception_v3_tf']}, {'models': ['inception_resnet_v2', 'resnet34'], 'weights': [1.0, 1.0]},
+        {'models': ['adv_inception_resnet_v2', 'inception_v3_tf']}, ]
+
+    val_dataset = datasets.ImageFolder(val_dir, transforms.Compose(
+        [transforms.Scale(int(math.floor(args.img_size / 0.875))), transforms.CenterCrop(args.img_size),
+            transforms.ToTensor()]))
+
+    defense_ensemble = ['adv_inception_resnet_v2', 'dpn68b_extra']  # FIXME argument
+    defense_cfgs = [config_from_string(s, output_fn='') for s in defense_ensemble]
+    if len(defense_ensemble) > 1:
+        defense_model = create_ensemble(defense_cfgs, None)
+    else:
+        defense_model = create_model_from_cfg(defense_cfgs[0])
+
+    if args.mt:
+        if len(defense_ensemble) > 1:
+            defense_model = multi_task.MultiTaskEnsemble(defense_model.models, use_features=False)
+        else:
+            defense_model = multi_task.MultiTask(defense_model)
+
+    train_adv_defense(args, defense_model, train_dataset, val_dataset, attack_model_cfgs, attack_cfgs)
+
+
+def train_adv_defense(args, defense_model, train_dataset, val_dataset, attack_model_cfgs, attack_cfgs):
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
     num_gpu = args.num_gpu
     if num_gpu == 1:
         input_devices = [0]
@@ -230,68 +241,14 @@ def main():
 
     master_output_device = output_devices[0]
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                       ])),
-        batch_size=args.batch_size, shuffle=True, num_workers=1, pin_memory=True)
-
-    val_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-        ])),
-        batch_size=args.batch_size, shuffle=False, num_workers=1, pin_memory=True)
-
-    attack_cfgs = [
-        {'attack_name': 'iterative', 'targeted': False, 'num_steps': 40, 'random_start': True, 'max_epsilon': 0.3*255.0, 'step_alpha':0.01*255.0},
-        {'attack_name': 'iterative', 'targeted': False, 'num_steps': 1, 'random_start': False, 'max_epsilon': 0.3*255.0},
-    ]
-
-    """
-    natural_models = [
-        create_model_from_cfg({'model_name': 'madry', 'checkpoint_file': 'madry.pth'}, dataset='mnist'),
-        create_model_from_cfg({'model_name': 'pytorch-example', 'checkpoint_file': 'pytex.pth'}, dataset='mnist'),
-        create_model_from_cfg({'model_name': 'modela', 'checkpoint_file': 'modela.pth'}, dataset='mnist'),
-        create_model_from_cfg({'model_name': 'modelb', 'checkpoint_file': 'modelb.pth'}, dataset='mnist'),
-        create_model_from_cfg({'model_name': 'modelc', 'checkpoint_file': 'modelc.pth'}, dataset='mnist'),
-        create_model_from_cfg({'model_name': 'modeld', 'checkpoint_file': 'modeld.pth'}, dataset='mnist'),
-    ]
-
-    ensemble = Ensemble(models=natural_models)
-
-    adversarial_model = create_model_from_cfg({'model_name': 'madry', 'checkpoint_file': 'madry_ens_adv.pth'}, dataset='mnist')
-    """
-
-
-    adv_generator = AdversarialGenerator(
-        train_loader,
-        model_cfgs=[],
-        attack_cfgs=attack_cfgs,
-        attack_probs=[0.5, 0.5],
-        output_batch_size=args.batch_size,
-        input_devices=input_devices,
+    adv_generator = AdversarialGenerator(train_loader, model_cfgs=attack_model_cfgs, attack_cfgs=attack_cfgs,
+        attack_probs=[0.4, 0.4, 0.1, 0.1], output_batch_size=args.batch_size, input_devices=input_devices,
         master_output_device=master_output_device)
-
-    adv_generator.models = []
 
     if args.mp:
         adv_generator = MpFeeder(adv_generator, maxsize=8)
 
     with torch.cuda.device(master_output_device):
-        defense_ensemble = [True]
-        defense_model = create_model_from_cfg(
-            {'model_name': 'madry', 'checkpoint_file': None},
-            dataset='mnist')
-
-        if args.mt:
-            if len(defense_ensemble) > 1:
-                defense_model = multi_task.MultiTaskEnsemble(
-                    defense_model.models,
-                    use_features=False)
-            else:
-                defense_model = multi_task.MultiTask(defense_model)
-
         if len(output_devices) > 1:
             defense_model = torch.nn.DataParallel(defense_model, output_devices).cuda()
         else:
@@ -301,15 +258,10 @@ def main():
             adv_generator.set_dogfood(defense_model)
 
         if args.opt == 'sgd':
-            optimizer = torch.optim.SGD(
-                get_opt_params(defense_model, classifier_only=args.co),
-                args.lr,
-                momentum=args.momentum,
-                weight_decay=args.weight_decay)
-        elif args.opt =='adam':
-            optimizer = torch.optim.Adam(
-                get_opt_params(defense_model, classifier_only=args.co),
-                args.lr,
+            optimizer = torch.optim.SGD(get_opt_params(defense_model, classifier_only=args.co), args.lr,
+                momentum=args.momentum, weight_decay=args.weight_decay)
+        elif args.opt == 'adam':
+            optimizer = torch.optim.Adam(get_opt_params(defense_model, classifier_only=args.co), args.lr,
                 weight_decay=args.weight_decay)
         else:
             assert False, "Invalid optimizer specified"
@@ -320,11 +272,10 @@ def main():
                 checkpoint = torch.load(args.resume)
                 if isinstance(checkpoint, dict) and 'state_dict' in checkpoint:
                     args.start_epoch = checkpoint['epoch']
-                    #best_prec1 = checkpoint['best_prec1']
+                    # best_prec1 = checkpoint['best_prec1']
                     defense_model.load_state_dict(checkpoint['state_dict'])
                     optimizer.load_state_dict(checkpoint['optimizer'])
-                    print("=> loaded checkpoint '{}' (epoch {})".format(
-                        args.resume, checkpoint['epoch']))
+                    print("=> loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint['epoch']))
                 else:
                     # load from a non-training state dict only checkpoint
                     defense_model.load_state_dict(checkpoint)
@@ -345,19 +296,13 @@ def main():
             # evaluate on validation set
             prec1 = validate(args, val_loader, defense_model, criterion)
 
-            #FIXME run another validation on all adversarial examples?
+            # FIXME run another validation on all adversarial examples?
 
             # remember best prec@1 and save checkpoint
             is_best = prec1 > best_prec1
             best_prec1 = max(prec1, best_prec1)
-            save_checkpoint({
-                    'epoch': epoch + 1,
-                    'arch': 'FIXME',
-                    'state_dict': defense_model.state_dict(),
-                    'best_prec1': best_prec1,
-                    'optimizer': optimizer.state_dict(),
-                },
-                is_best,
+            save_checkpoint({'epoch': epoch + 1, 'arch': 'FIXME', 'state_dict': defense_model.state_dict(),
+                'best_prec1': best_prec1, 'optimizer': optimizer.state_dict(), }, is_best,
                 filename='checkpoint-%d.pth.tar' % epoch)
 
     if args.mp:
@@ -413,5 +358,3 @@ def accuracy(output, target, topk=(1,)):
 
 if __name__ == '__main__':
     main()
-
-
