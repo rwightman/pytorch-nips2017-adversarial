bc21e3b1cbcc9fa1c6bd40b83000b74dac83d89c
alekseynp <me@alekseynp.com>
2017-10-12 00:08:28 -0700

    Added post_n to Ensemble, enables ensembling logits or probs, but then outputting logprobs.

--- python/models/ensemble.py
+++ python/models/ensemble.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 
 class Ensemble(nn.Module):
-    def __init__(self, models, ensembling_weights=None):
+    def __init__(self, models, ensembling_weights=None, post_fn=None):
         super(Ensemble, self).__init__()
 
         self.models = nn.ModuleList(models)
@@ -12,15 +12,20 @@ class Ensemble(nn.Module):
         else:
             self.ensembling_weights = ensembling_weights
 
+        self.post_fn = post_fn
+
     def forward(self, x):
         outputs = [model(x) for model in self.models]
 
         o = self.ensembling_weights[0] * outputs[0]
         for idx in range(len(outputs)-1):
             o = o + self.ensembling_weights[idx + 1]*outputs[idx+1]
-        mean_probs = o / sum(self.ensembling_weights)
+        out = o / sum(self.ensembling_weights)
 
-        return mean_probs
+        if self.post_fn is not None:
+            out = self.post_fn(out)
+
+        return out
 
     def classifier_parameters(self):
         params = []
