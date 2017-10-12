87def62818802620ab8460617f5b420cd4959b53
alekseynp <me@alekseynp.com>
2017-10-12 00:08:58 -0700

    more cifar models

--- python/models/cifar/model_factory.py
+++ python/models/cifar/model_factory.py
@@ -1,4 +1,6 @@
-from .torchvision import Resnet18
+from .torchvision import Resnet18, Resnet34
+from .wide_resnet import Wide_ResNet
+
 from models.load_checkpoint import load_checkpoint
 from models.transformed_model import TransformedModel
 from processing import Normalize
@@ -12,11 +14,14 @@ def create_model(
 
     if model_name == 'resnet18':
         model = Resnet18(num_classes)
+    elif model_name == 'resnet34':
+        model = Resnet34(num_classes)
+    elif model_name == 'wr16x4':
+        model = Wide_ResNet(16, 4, 0.3, num_classes)
+    elif model_name == 'wr40x4':
+        model = Wide_ResNet(40, 4, 0.3, num_classes)
     else:
         raise ValueError('Invalid model_name: {}'.format(model_name))
-
-    if checkpoint_path:
-        load_checkpoint(model, checkpoint_path)
 
     if num_classes == 10:
         normalizer = Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
@@ -31,6 +36,9 @@ def create_model(
         normalizer=normalizer,
         output_fn=output_fn
     )
+
+    if checkpoint_path:
+        load_checkpoint(model, checkpoint_path)
 
     return model
 
